808cbeeedef441200c6cc8ccf6580ef896a53e78
Nik <niklas.varoquier@rwth-aachen.de>
2021-03-01 08:37:59 +0100

    First alpha version

---
+++ data/mega_evolution_settings.js
@@ -0,0 +1,8 @@
+const pokemon_mega = {
+    "general_attack_boost": 1.1,
+    "max_mega_candy": 2000,
+    "mega_evolution_bonus_catch_candy": 1,
+    "mega_evolution_duration": 28800000,
+    "same_type_attack_boost": 1.3,
+    "walking_buddy_gives_mega_energy": true
+}
---
+++ data/weather_boosts.js
@@ -0,0 +1,44 @@
+const pokemon_weather = [
+    {
+    "weather" : "Clear",
+    "boost" : [
+        "Grass",
+        "Ground",
+        "Fire"
+    ]},{
+        "weather" : "Fog",
+        "boost" : [
+        "Dark",
+        "Ghost"
+    ]},{
+        "weather" :"Overcast",
+        "boost" : [
+        "Fairy",
+        "Fighting",
+        "Poison"
+    ]},{
+        "weather" :"Partly Cloudy",
+        "boost" : [
+        "Normal",
+        "Rock"
+    ]},{
+        "weather" : "Rainy",
+        "boost" : [
+        "Water",
+        "Electric",
+        "Bug"
+    ]},{
+        "weather" : "Snow",
+        "boost" : [
+        "Ice",
+        "Steel"
+    ]},
+    {
+        "weather" : "Windy",
+        "boost" : [
+        "Dragon",
+        "Flying",
+        "Psychic"
+    ]
+}
+]
